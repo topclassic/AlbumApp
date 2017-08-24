87b1554ae37bd9862dffd676c33dc90a45d4cff8
Chotipat Top <topz.classic@gmail.com>
2017-08-24 16:52:06 +0700

    Use Flexbox set column in CardSection

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,27 +3,31 @@ import React, { Component } from 'react';
 import Card from './Card'
 import CardSection from './CardSection'
 import { 
+    View,
     Text, 
     StyleSheet 
 } from 'react-native';
 
 const AlbumDetail = (props) =>{
-    const {title} = props.album
+    const {title, artist} = props.album
+    const {headerStyle} = styles
     return(
         <Card>
             <CardSection>
-                <Text>
-                    {title}
-                </Text>
-            </CardSection>
-
-            <CardSection>
-                <Text>
-                    {title}
-                </Text>
+            
+                <View style={headerStyle}>
+                <Text>{title}</Text>
+                <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     )
 }
+const styles = StyleSheet.create({
+    headerStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+})
 //make this component available to the app
 export default AlbumDetail;
